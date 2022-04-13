c6deb75fe4b3ed9e0b14c2e89530c0119cc0919f
Gregory Ostermayr <gregory.ostermayr@gmail.com>
2022-04-13 18:30:05 -0400

    pad binary tree, add big test
    formatting

--- test/merkle_fun_test.exs
+++ test/merkle_fun_test.exs
@@ -81,7 +81,7 @@ defmodule MerkleFunTest do
       "6772a13d0dc3e75fdc0e39d1f6810ce673fc00495c0544aa8e39f09a35ab4770",
       "b1602e2aab98d8de4e719cd1f574bde135dae520a74b2d41d441ccef5e3f037e",
       "e661403df734ea32520cf963848afaf7fd07cd92765eb51a3d611f4bdc4a8073",
-      "fb800734b01c1c5c1d15e4b79bf150c335fb7f93554e92ebb452a09440a6f5e5",
+      "fb800734b01c1c5c1d15e4b79bf150c335fb7f93554e92ebb452a09440a6f5e5"
     ]
 
     mt = MerkleFun.new(@seven_addresses)
@@ -115,30 +115,36 @@ defmodule MerkleFunTest do
       "0xdd46f9887cc3e2bf747ba7c44088af72afb7e7ef8a9979aa46bd2d8bba27eccc",
       "0x32dc8ca919f2d035e7f16dcde4a27643ec570605d81dc0fe025f55eeb1591dcf"
     ]
-    b_expected =  [
+
+    b_expected = [
       "0xb19babd29e7815b696f28283e6ac98b9ac6f765ac5e2c3d7242fcdb9bb52a3e4",
       "0x765fef4f7022a8741f699b8601560d7bb1fcd3fd18ef356bf0558a34c618723b"
     ]
+
     c_expected = [
       "0x6772a13d0dc3e75fdc0e39d1f6810ce673fc00495c0544aa8e39f09a35ab4770",
       "0x6ea40cfb86421f8e6264f14be34afa63efd4b6bd03d17687d803fa604bc8c4d5",
       "0x32dc8ca919f2d035e7f16dcde4a27643ec570605d81dc0fe025f55eeb1591dcf"
     ]
+
     d_expected = [
       "0xe661403df734ea32520cf963848afaf7fd07cd92765eb51a3d611f4bdc4a8073",
       "0xfb800734b01c1c5c1d15e4b79bf150c335fb7f93554e92ebb452a09440a6f5e5",
       "0x765fef4f7022a8741f699b8601560d7bb1fcd3fd18ef356bf0558a34c618723b"
     ]
+
     e_expected = [
       "0xb1602e2aab98d8de4e719cd1f574bde135dae520a74b2d41d441ccef5e3f037e",
       "0xfb800734b01c1c5c1d15e4b79bf150c335fb7f93554e92ebb452a09440a6f5e5",
       "0x765fef4f7022a8741f699b8601560d7bb1fcd3fd18ef356bf0558a34c618723b"
     ]
+
     f_expected = [
       "0x114dfe5ced5f046afff8bd1e24f55b3423eff53de2ef131e3c2218266edf2a55",
       "0xdd46f9887cc3e2bf747ba7c44088af72afb7e7ef8a9979aa46bd2d8bba27eccc",
       "0x32dc8ca919f2d035e7f16dcde4a27643ec570605d81dc0fe025f55eeb1591dcf"
     ]
+
     g_expected = [
       "0x52a78285a25d74e30043be50c247c58c402e121312ed596deabafba40c2dc667",
       "0x6ea40cfb86421f8e6264f14be34afa63efd4b6bd03d17687d803fa604bc8c4d5",
@@ -155,5 +161,34 @@ defmodule MerkleFunTest do
     assert MerkleFun.proof(mt, e) === e_expected
     assert MerkleFun.proof(mt, f) === f_expected
     assert MerkleFun.proof(mt, g) === g_expected
+  end
+
+  test "big test" do
+    expected = [
+      "0x11481edda7910f8b360477264c7d95efdbbd6c54865b037826116f1381be931c",
+      "0xff95de80beb93f114e513fde55bb56aaf97235b88538e44868386fde8ccdd332",
+      "0x8b408b46a623e32f2d96cfddf9dbe99ece434616176c88fcaf83b95400990c9c",
+      "0x153a7b112244f82b2b7bb1da7aef9575daf7cd6d627348ef5f3fa61b7190623f",
+      "0x280988f017470994b78a7d4e7ca9c9a576c87617b08718b431d6b0e31334fa5d",
+      "0x6a886c782ce372011493b26dad44792edce43a55419b712e303e7829257c2610",
+      "0xf57cdf09c8dd33cfa2fd1f5a295e05896a05de60c763071bcff683b26de01947",
+      "0x52a88fff99be8327601afcd937c28ea226d31b80cc428679fad2a05200648622",
+      "0x5609be6fcc1286912b191ef479e94817df95221f734f3d779eaf9d1b922cf017",
+      "0xd266d1f339829e566da76e6d8cf46c72d17a7c3ad876dd885fd88031e6b76038",
+      "0xc37fc574462a5112a0e0792f52a7e84e888662f94fba6fe895a7c93fb0c63b34",
+      "0x72b7cebabda59238c211cd17dec39c97e2f482d88f6ec1c3fb3bdc3ab03bd208"
+    ]
+
+    {:ok, data} = File.read("./test/test_addresses.txt")
+
+    proof =
+      data
+      |> String.split()
+      |> Enum.map(fn "0x" <> address -> address end)
+      |> MerkleFun.new()
+      # |> :erlang.term_to_binary() |> :erlang.byte_size() |> IO.inspect
+      |> MerkleFun.proof("8a4f26aa310cfaa0bfb679572bac264e10b83b7b")
+
+    assert proof == expected
   end
 end
