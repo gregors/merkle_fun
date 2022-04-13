c4f8977c6f8ed319f32b7c7821b4ac1a77ca9736
Rodger Maarfi <acrite22@hotmail.com>
2022-04-13 11:43:14 -0400

    add test for new

--- test/merkle_fun_test.exs
+++ test/merkle_fun_test.exs
@@ -12,6 +12,15 @@ defmodule MerkleFunTest do
     "0B206F63e2386c20C25b6901928DB3134Dc6ec51"
     | ["bf4De19Bd8Bf01e1503d4E8d7a3Ab75451dd653d" | @three_addresses]
   ]
+  @seven_addresses [
+    "8a4f26aa310cfaa0bfb679572bac264e10b83b7b",
+    "bd1db599387cefe8cb1bb930f2893397d0c7360e",
+    "437306814bd44e00c3050848a630acf00c73705d",
+    "aa2e8370515ff6f6d9bb2193e78a0f029f69e648",
+    "7e5866042a595197545b8855cbe54d12b6b00e98",
+    "6ae07f738c81b580ff537ffe266f4fc298e3272c",
+    "44958629e8a217569a474f382512949cfb386146"
+  ]
 
   test ".new - with even leaves" do
     expected = [
@@ -54,6 +63,28 @@ defmodule MerkleFunTest do
     ]
 
     mt = MerkleFun.new(@five_addresses)
+    assert MerkleFun.print(mt) === expected
+  end
+
+  test ".new - with 7 leaves" do
+    expected = [
+      "303ceaf1723e734239e6277389e335dceb491d970f0939df5442f3e1b3d2e925",
+      "765fef4f7022a8741f699b8601560d7bb1fcd3fd18ef356bf0558a34c618723b",
+      "32dc8ca919f2d035e7f16dcde4a27643ec570605d81dc0fe025f55eeb1591dcf",
+      "6ea40cfb86421f8e6264f14be34afa63efd4b6bd03d17687d803fa604bc8c4d5",
+      "dd46f9887cc3e2bf747ba7c44088af72afb7e7ef8a9979aa46bd2d8bba27eccc",
+      "b19babd29e7815b696f28283e6ac98b9ac6f765ac5e2c3d7242fcdb9bb52a3e4",
+      "fb800734b01c1c5c1d15e4b79bf150c335fb7f93554e92ebb452a09440a6f5e5",
+      "0e812e91ffbecace4dd96070cf2a3ae3ccfa4cabcd5b5521a6631b64feeeb5e7",
+      "114dfe5ced5f046afff8bd1e24f55b3423eff53de2ef131e3c2218266edf2a55",
+      "52a78285a25d74e30043be50c247c58c402e121312ed596deabafba40c2dc667",
+      "6772a13d0dc3e75fdc0e39d1f6810ce673fc00495c0544aa8e39f09a35ab4770",
+      "b1602e2aab98d8de4e719cd1f574bde135dae520a74b2d41d441ccef5e3f037e",
+      "e661403df734ea32520cf963848afaf7fd07cd92765eb51a3d611f4bdc4a8073",
+      "fb800734b01c1c5c1d15e4b79bf150c335fb7f93554e92ebb452a09440a6f5e5",
+    ]
+
+    mt = MerkleFun.new(@seven_addresses)
     assert MerkleFun.print(mt) === expected
   end
 
